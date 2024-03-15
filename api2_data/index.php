5cbc8711ca55ac291427d1e93197a46130222867
Ryan <mordeckai.pro@gmail.com>
2024-03-15 10:06:15 +0100

    base application

--- api2_data/index.php
+++ api2_data/index.php
@@ -1,12 +1,30 @@
 <?php
 
-// Looing for .env at the root directory
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+$method = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
 
-// Retrive env variable
-$userName = $_ENV['DB_DATABASE'];
+switch ($method || $uri) {
+    case ($method == 'POST' && $uri == "/api/authentification"):
+        header('Content-Type: application/json');
+        $data = json_decode(file_get_contents('php://input'), true);
 
-echo $userName;
+        if (!empty($data['login']) && !empty($data['mdp'])) {
+            echo AuthentificationAPI($data['login'], $data['mdp']);
+        } else {
+            echo json_encode(['error' => "Les informations d'authentification sont incorrectes"]);
+        }
+        
+        break;
+    case ($method == 'GET' && str_contains($uri, '/api/authentification')):
+        header('Content-Type: application/json');
+        preg_match_all('/\/api\/authentification\/[\s\S]+/', $uri, $matches);
+        echo var_dump($matches);
+        // echo ValidTokenAPI($jwt);
+        break;
+    default:
+        http_response_code(404);
+        echo json_encode(['error' => "We cannot find what you're looking for."]);
+        break;
+}
 
 ?>
